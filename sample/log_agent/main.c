c9c4dd0c033783bbc6c25748ed9dd6ccd9c52cc9
k.owari <iamowari@gmail.com>
2017-09-18 15:32:42 +0000

    update logd

--- sample/log_agent/main.c
+++ sample/log_agent/main.c
@@ -30,11 +30,210 @@
 #include "gdt_random.h"
 
 GDT_MEMORY_POOL* main_memory_pool = NULL;
+int32_t g_reciver_munit;
+
+#define READ_SIZE SIZE_KBYTE*32
+#define AGENT_BUF_SIZE SIZE_KBYTE*514
+#define RECIVER_STATUS_DEFAULT 0
+#define RECIVER_STATUS_RECIVE 1
+#define RECIVER_STATUS_WAIT 2
+
+#define GDT_LOG_AGENT_TARGET_STATUS_DEFAULT 0
+#define GDT_LOG_AGENT_TARGET_STATUS_RUNNING 1
+
+#define MAX_LOG_AGENT_TARGETS 8
+
+#define LOG_PATH_TYPE_ERROR 0x01
+#define LOG_PATH_TYPE_INFO  0x02
+#define LOG_PATH_TYPE_DEBUG 0x04
+
+typedef struct GDT_LOG_AGENT_TARGET
+{
+	GDT_MEMORY_POOL* memory_pool;
+	int32_t path_munit;
+	int32_t agent_buf_munit;
+	int32_t agent_buf_pos;
+	size_t current_pos;
+	int64_t filetime;
+	int status;
+} GDT_LOG_AGENT_TARGET;
+
+typedef struct GDT_LOG_RECIVER
+{
+	GDT_MEMORY_POOL* memory_pool;
+	int32_t log_targets_munit;
+	int log_targets_length;
+	int status;
+} GDT_LOG_RECIVER;
+
+int32_t make_log_reciver(GDT_MEMORY_POOL* memory_pool,size_t length);
+int32_t init_log_reciver(GDT_LOG_RECIVER* reciver);
+int32_t init_log_agent_target(GDT_LOG_AGENT_TARGET* target);
+int32_t make_date_log_path(GDT_MEMORY_POOL* memory_pool,int32_t dest_munit,char* base_path,int type);
+int32_t make_log_path(GDT_MEMORY_POOL* memory_pool,int32_t dest_munit,char* path);
+
+
+int32_t add_log_agent_target(uint32_t payload_type, uint8_t* payload, size_t payload_len, GDT_RECV_INFO *gdt_recv_info);
+int32_t add_log_agent_target(uint32_t payload_type, uint8_t* payload, size_t payload_len, GDT_RECV_INFO *gdt_recv_info)
+{
+	GDT_LOG_RECIVER* recivers = (GDT_LOG_RECIVER*)GDT_POINTER(main_memory_pool,g_reciver_munit);
+	GDT_LOG_RECIVER* reciver = (recivers+gdt_recv_info->tinfo->index);
+	GDT_LOG_AGENT_TARGET* targets = (GDT_LOG_AGENT_TARGET*)GDT_POINTER(reciver->memory_pool,reciver->log_targets_munit);
+	GDT_LOG_AGENT_TARGET* target = NULL;
+	char* pbuf = (char*)payload;
+	if( !strcmp(pbuf,"date_log_error") || !strcmp(pbuf,"date_log")){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_date_log_path(target->memory_pool,target->path_munit,"./",LOG_PATH_TYPE_ERROR);
+	}
+	if( !strcmp(pbuf,"date_log_info") || !strcmp(pbuf,"date_log")){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_date_log_path(target->memory_pool,target->path_munit,"./",LOG_PATH_TYPE_INFO);
+	}
+	if( !strcmp(pbuf,"date_log_debug") || !strcmp(pbuf,"date_log")){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_date_log_path(target->memory_pool,target->path_munit,"./",LOG_PATH_TYPE_DEBUG);
+	}
+	if( !strcmp(pbuf,"apache_error_log") ){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_log_path(target->memory_pool,target->path_munit,"/var/log/httpd/error.log");
+	}
+	if( !strcmp(pbuf,"apache_access_log") ){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_log_path(target->memory_pool,target->path_munit,"/var/log/httpd/access.log");
+	}
+	if( !strcmp(pbuf,"nginx_error_log") ){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_log_path(target->memory_pool,target->path_munit,"/var/log/nginx/error.log");
+	}
+	if( !strcmp(pbuf,"nginx_access_log") ){
+		target = targets+reciver->log_targets_length++;
+		target->status = GDT_LOG_AGENT_TARGET_STATUS_RUNNING;
+		target->current_pos = 0;
+		target->filetime = 0;
+		make_log_path(target->memory_pool,target->path_munit,"/var/log/nginx/access.log");
+	}
+	reciver->status = RECIVER_STATUS_RECIVE;
+	return GDT_SYSTEM_OK;
+}
 
 // callback
 int on_connect(GDT_SERVER_CONNECTION_INFO* connection);
 int32_t on_payload_recv(uint32_t payload_type, uint8_t* payload, size_t payload_len, GDT_RECV_INFO *gdt_recv_info);
 int on_close(GDT_SERVER_CONNECTION_INFO* connection);
+
+int32_t make_log_reciver(GDT_MEMORY_POOL* memory_pool,size_t length)
+{
+	int32_t reciver_munit = gdt_create_munit(memory_pool,sizeof(GDT_LOG_RECIVER)*length,MEMORY_TYPE_DEFAULT);
+	GDT_LOG_RECIVER* recivers = (GDT_LOG_RECIVER*)GDT_POINTER(memory_pool,reciver_munit);
+	GDT_LOG_AGENT_TARGET* targets = NULL;
+	int i,j;
+	for( i = 0; i < length; i++ ){
+		(recivers+i)->memory_pool = memory_pool;
+		(recivers+i)->status = RECIVER_STATUS_DEFAULT;
+		(recivers+i)->log_targets_munit = gdt_create_munit(memory_pool,sizeof(GDT_LOG_AGENT_TARGET)*MAX_LOG_AGENT_TARGETS,MEMORY_TYPE_DEFAULT);
+		(recivers+i)->log_targets_length = 0;
+		targets = (GDT_LOG_AGENT_TARGET*)GDT_POINTER(memory_pool,(recivers+i)->log_targets_munit);
+		for(j=0;j<MAX_LOG_AGENT_TARGETS;j++){
+			(targets+j)->memory_pool = memory_pool;
+			(targets+j)->path_munit = gdt_create_munit(main_memory_pool,MAXPATHLEN,MEMORY_TYPE_DEFAULT);
+			(targets+j)->agent_buf_munit = gdt_create_munit(main_memory_pool,AGENT_BUF_SIZE,MEMORY_TYPE_DEFAULT);
+			(targets+j)->agent_buf_pos = 0;
+			(targets+j)->current_pos = 0;
+			(targets+j)->filetime = 0;
+			(targets+j)->status = GDT_LOG_AGENT_TARGET_STATUS_DEFAULT;
+		}
+	}
+	return reciver_munit;
+}
+
+int32_t init_log_reciver(GDT_LOG_RECIVER* reciver)
+{
+	GDT_LOG_AGENT_TARGET* targets = NULL;
+	int j;
+	reciver->status = RECIVER_STATUS_DEFAULT;
+	reciver->log_targets_length = 0;
+	targets = (GDT_LOG_AGENT_TARGET*)GDT_POINTER(reciver->memory_pool,reciver->log_targets_munit);
+	for(j=0;j<MAX_LOG_AGENT_TARGETS;j++){
+		init_log_agent_target((targets+j));
+	}
+	return GDT_SYSTEM_OK;
+}
+
+int32_t init_log_agent_target(GDT_LOG_AGENT_TARGET* target)
+{
+	char* path = (char*)GDT_POINTER(target->memory_pool,target->path_munit);
+	memset(path,0,gdt_usize(target->memory_pool,target->path_munit));
+	char* buf = (char*)GDT_POINTER(target->memory_pool,target->agent_buf_munit);
+	memset(buf,0,gdt_usize(target->memory_pool,target->agent_buf_munit));
+	target->agent_buf_pos = 0;
+	target->current_pos = 0;
+	target->filetime = 0;
+	target->status = GDT_LOG_AGENT_TARGET_STATUS_DEFAULT;
+	return GDT_SYSTEM_OK;
+}
+
+int32_t make_date_log_path(GDT_MEMORY_POOL* memory_pool,int32_t dest_munit,char* base_path,int type)
+{
+	if( dest_munit == -1 ){
+		return -1;
+	}
+	char* path = (char*)GDT_POINTER(memory_pool,dest_munit);
+	size_t path_buffer_len = gdt_usize(memory_pool,dest_munit);
+	time_t nowtime = time(NULL);
+	struct tm t;
+	char date[20];
+#ifdef __WINDOWS__
+	localtime_s(&t, &nowtime);
+#else
+	localtime_r(&nowtime, &t);
+#endif
+	strftime( date, sizeof(date), "%Y%m%d", &t );
+	size_t path_len = 0;
+	path_len = gdt_strlink( path, 0, base_path, gdt_strlen(base_path), path_buffer_len );
+	if( type == LOG_PATH_TYPE_ERROR ){
+		path_len = gdt_strlink( path, path_len, "error", 5, path_buffer_len );
+	}
+	if( type == LOG_PATH_TYPE_INFO ){
+		path_len = gdt_strlink( path, path_len, "info", 4, path_buffer_len );
+	}
+	if( type == LOG_PATH_TYPE_DEBUG ){
+		path_len = gdt_strlink( path, path_len, "debug", 5, path_buffer_len );
+	}
+	path_len = gdt_strlink( path, path_len, "_", 1, path_buffer_len );
+	path_len = gdt_strlink( path, path_len, date, gdt_strlen(date), path_buffer_len );
+	path_len = gdt_strlink( path, path_len, ".log", 4, path_buffer_len );
+	return dest_munit;
+}
+
+int32_t make_log_path(GDT_MEMORY_POOL* memory_pool,int32_t dest_munit,char* path)
+{
+	if( dest_munit == -1 ){
+		return -1;
+	}
+	char* dest = (char*)GDT_POINTER(memory_pool,dest_munit);
+	size_t path_buffer_len = gdt_usize(memory_pool,dest_munit);
+	size_t path_len = 0;
+	path_len = gdt_strlink( dest, 0, path, gdt_strlen(path), path_buffer_len );
+	return dest_munit;
+}
 
 int on_connect(GDT_SERVER_CONNECTION_INFO* connection)
 {
@@ -43,10 +242,18 @@ int on_connect(GDT_SERVER_CONNECTION_INFO* connection)
 
 int32_t on_payload_recv(uint32_t payload_type, uint8_t* payload, size_t payload_len, GDT_RECV_INFO *gdt_recv_info)
 {
-	gdt_print_payload(payload_type,payload,payload_len,128);
+	//gdt_print_payload(payload_type,payload,payload_len,128);
+	GDT_LOG_RECIVER* recivers = NULL;
+	GDT_LOG_RECIVER* reciver = NULL;
 	switch( payload_type )
 	{
 		case 0x01:
+			add_log_agent_target(payload_type, payload, payload_len, gdt_recv_info);
+			break;
+		case 0x02:
+			recivers = (GDT_LOG_RECIVER*)GDT_POINTER(main_memory_pool,g_reciver_munit);
+			reciver = (recivers+gdt_recv_info->tinfo->index);
+			reciver->status = RECIVER_STATUS_RECIVE;
 			break;
 		default:
 			// invalid packet
@@ -57,6 +264,9 @@ int32_t on_payload_recv(uint32_t payload_type, uint8_t* payload, size_t payload_
 
 int on_close(GDT_SERVER_CONNECTION_INFO* connection)
 {
+	GDT_LOG_RECIVER* recivers = (GDT_LOG_RECIVER*)GDT_POINTER(main_memory_pool,g_reciver_munit);
+	GDT_LOG_RECIVER* reciver = (recivers+connection->index);
+	init_log_reciver(reciver);
 	return 0;
 }
 
@@ -68,7 +278,7 @@ int main( int argc, char *argv[], char *envp[] )
 	//gdt_daemonize( 0 , 0 );
 	gdt_srand_32();
 	do{
-		if( -1 == gdt_initialize_memory_f64(&main_memory_pool,SIZE_MBYTE*1)){
+		if( -1 == gdt_initialize_memory_f64(&main_memory_pool,SIZE_MBYTE*32)){
 			printf( "gdt_initialize_memory_f64 error\n" );
 			break;
 		}
@@ -76,8 +286,8 @@ int main( int argc, char *argv[], char *envp[] )
 		char* portnum = "37012";
 		GDT_MEMORY_POOL* memory_pool = NULL;
 		GDT_SOCKET_OPTION *option = NULL;
-		size_t maxconnection = 1;
-		if (gdt_initialize_memory_f64(&memory_pool, SIZE_MBYTE*1) <= 0) {
+		size_t maxconnection = 4;
+		if (gdt_initialize_memory_f64(&memory_pool, SIZE_MBYTE*8) <= 0) {
 			printf("gdt_initialize_memory error\n");
 			break;
 		}
@@ -89,68 +299,131 @@ int main( int argc, char *argv[], char *envp[] )
 		if (0 != gdt_initialize_socket_option(option, hostname, portnum, SOKET_TYPE_SERVER_TCP, SOCKET_MODE_NONBLOCKING, PROTOCOL_SIMPLE, maxconnection, memory_pool, NULL)) {
 			break;
 		}
-		set_message_buffer(option,SIZE_KBYTE*32);
+		set_message_buffer(option,SIZE_KBYTE*512);
 		set_on_connect_event( option, on_connect);
 		set_on_payload_recv_event(option, on_payload_recv);
 		set_on_close_event( option, on_close );
 		gdt_socket(option);
+
+		g_reciver_munit = make_log_reciver(main_memory_pool,maxconnection);
+		if( g_reciver_munit == -1 ){
+			break;
+		}
+		GDT_LOG_RECIVER* recivers = (GDT_LOG_RECIVER*)GDT_POINTER(main_memory_pool,g_reciver_munit);
+		GDT_LOG_AGENT_TARGET* targets = NULL;
+		int i,j;
+		char* current_path = NULL;
+		char* pbuf = NULL;
+		GDT_FILE_INFO finfo;
+		int sleeptime = 20000;
+		int execcount = 0;
+		int execcount_total = 0;
+		uint32_t sleepcnt = 0;
+
 		int32_t sync_timer = time(NULL);
 		int32_t current_time = 0;
-		int32_t base_path_munit = gdt_create_munit(main_memory_pool,MAXPATHLEN,MEMORY_TYPE_DEFAULT);
-		int32_t current_path_munit = gdt_create_munit(main_memory_pool,MAXPATHLEN,MEMORY_TYPE_DEFAULT);
-		int32_t next_path_munit = gdt_create_munit(main_memory_pool,MAXPATHLEN,MEMORY_TYPE_DEFAULT);
-
-		if( argc <= 1 ){
-			printf("argument : log_path\n");
-			break;
-		}
-		char* base_path = (char*)GDT_POINTER(main_memory_pool,base_path_munit);
-		gdt_strcopy(base_path,argv[1],gdt_usize(main_memory_pool,base_path_munit));
-		size_t base_path_len = gdt_strlen(base_path);
-		if( base_path_len == 0 ){
-			printf("invalid log_path");
-			break;
-		}
-		if( base_path[base_path_len-1] != '/' ){
-			base_path[base_path_len] = '/';
-			base_path[base_path_len+1] = '\0';
-		}
-		base_path_len = gdt_strlen(base_path);
-		time_t nowtime = time(NULL);
-		struct tm t;
-		char date[20];
-#ifdef __WINDOWS__
-		localtime_s(&t, &nowtime);
-#else
-		localtime_r(&nowtime, &t);
-#endif
-		strftime( date, sizeof(date), "%Y%m%d", &t );
-		size_t path_len = 0;
-		char* current_path = (char*)GDT_POINTER(main_memory_pool,current_path_munit);
-		path_len = gdt_strlink( current_path, 0, base_path, gdt_strlen(base_path), gdt_usize(main_memory_pool,current_path_munit) );
-		path_len = gdt_strlink( current_path, path_len, "error_", 6, gdt_usize(main_memory_pool,current_path_munit) );
-		path_len = gdt_strlink( current_path, path_len, date, gdt_strlen(date), gdt_usize(main_memory_pool,current_path_munit) );
-		path_len = gdt_strlink( current_path, path_len, ".log", 4, gdt_usize(main_memory_pool,current_path_munit) );
-		GDT_FILE_INFO finfo;
-		finfo.size=0;
-		size_t old_size = 0;
 		while (1) {
 			gdt_server_update(option);
-			usleep(100000); // wait 1ms
 			current_time = time(NULL);
-			if( current_time - sync_timer > 1 ){
+			if( current_time - sync_timer > 3 ){
 				sync_timer = current_time;
+				if( execcount_total+execcount == 0 ){
+					sleeptime = 300000;
+					sleepcnt++;
+				}
+				execcount_total=0;
 			}
-			gdt_fget_info(current_path,&finfo);
-			// printf("file size : %d\n",(int)finfo.size);
-			if( old_size < finfo.size ){
-				//printf("update!!\n");
-				char buf[64];
-				size_t read_size = gdt_fread_range(current_path,buf,old_size,sizeof(buf));
-				//printf("read(%d->%d) : %s\n",(int)old_size,(int)(old_size+read_size),buf);
-				//printf("%s",buf);
-				gdt_send_broadcast(option,buf,gdt_strlen(buf),0x01);
-				old_size += read_size;
+			execcount_total+=execcount;
+			if(execcount>0){
+				sleeptime = 20000;
+				sleepcnt = 0;
+			}
+			else if( execcount_total > 0 ){
+				sleeptime = 100000;
+			}
+			usleep(sleeptime);
+			execcount = 0;
+			for( i = 0; i < maxconnection; i++ ){
+				if( (recivers+i)->status == RECIVER_STATUS_RECIVE )
+				{
+					targets = (GDT_LOG_AGENT_TARGET*)GDT_POINTER((recivers+i)->memory_pool,(recivers+i)->log_targets_munit);
+					for(j=0;j<MAX_LOG_AGENT_TARGETS;j++){
+						if( (targets+j)->status == GDT_LOG_AGENT_TARGET_STATUS_RUNNING ){
+							current_path = (char*)GDT_POINTER((targets+j)->memory_pool,(targets+j)->path_munit);
+							finfo.size=0;
+							gdt_fget_info(current_path,&finfo);
+							if( (targets+j)->current_pos+(targets+j)->agent_buf_pos < finfo.size 
+								|| ( finfo.size > 0 && finfo.update_usec > (targets+j)->filetime ) 
+							){
+								if( (targets+j)->current_pos+(targets+j)->agent_buf_pos >= finfo.size )
+								{
+									if( finfo.update_usec > (targets+j)->filetime && (targets+j)->filetime > 0 ){
+										(targets+j)->current_pos = 0;
+										(targets+j)->agent_buf_pos = 0;
+										printf("file time update %lld (%s)\n",finfo.update_usec,current_path);
+									}
+								}
+								(targets+j)->filetime = finfo.update_usec;
+								pbuf = (char*)GDT_POINTER((targets+j)->memory_pool,(targets+j)->agent_buf_munit);
+								pbuf+=(targets+j)->agent_buf_pos;
+								size_t read_size = gdt_fread_bin_range(current_path,pbuf,(targets+j)->current_pos+(targets+j)->agent_buf_pos,READ_SIZE);
+								int32_t line_check = (int32_t)(read_size-1);
+								while(pbuf[line_check] != '\n'){
+									line_check--;
+									if( line_check < 0 ){
+										break;
+									}
+								}
+								if( line_check >= 0 
+									|| (targets+j)->agent_buf_pos >= AGENT_BUF_SIZE-READ_SIZE-1 
+									|| (targets+j)->current_pos + (targets+j)->agent_buf_pos + read_size >= finfo.size
+								)
+								{
+									if( (targets+j)->current_pos + (targets+j)->agent_buf_pos + read_size >= finfo.size )
+									{
+										(targets+j)->agent_buf_pos+=read_size;
+										pbuf[read_size] = '\0';
+									}
+									else if( line_check >= 0 ){
+										(targets+j)->agent_buf_pos+=line_check+1;
+										pbuf[line_check+1] = '\0';
+									}
+									else{
+										(targets+j)->agent_buf_pos+=read_size;
+										pbuf[read_size] = '\0';
+									}
+									GDT_SERVER_CONNECTION_INFO *tmptinfo = gdt_offsetpointer( option->memory_pool, option->connection_munit, sizeof( GDT_SERVER_CONNECTION_INFO ), i );
+									if( tmptinfo->sockparam.acc > 0 )
+									{
+										ssize_t ret = 0;
+										pbuf = (char*)GDT_POINTER((targets+j)->memory_pool,(targets+j)->agent_buf_munit);
+										if( -1 == ( ret = gdt_send( option, &tmptinfo->sockparam, pbuf, gdt_strlen(pbuf), 0x01 ) ) ){
+											if( option->close_callback != NULL ){
+												option->close_callback( tmptinfo );
+											}
+											gdt_free_sockparam( option, &tmptinfo->sockparam );
+										}
+										else{
+											(recivers+i)->status = RECIVER_STATUS_WAIT;
+										}
+										//printf("send(%d) : %d/%d\n",(int)ret,(int)((targets+j)->current_pos+(targets+j)->agent_buf_pos),(int)finfo.size);
+										if( (int)((targets+j)->current_pos+(targets+j)->agent_buf_pos) == (int)finfo.size ){
+											printf("send(%d) : %d/%d\n",(int)sleeptime,(int)((targets+j)->current_pos+(targets+j)->agent_buf_pos),(int)finfo.size);
+										}
+									}
+									(targets+j)->current_pos += (targets+j)->agent_buf_pos;
+									(targets+j)->agent_buf_pos = 0;
+								}
+								else{
+									(targets+j)->agent_buf_pos+=read_size;
+									//printf("buffer(%d) : %d/%d\n",(int)read_size,(int)((targets+j)->current_pos+(targets+j)->agent_buf_pos),(int)finfo.size);
+								}
+								execcount++;
+								usleep(sleeptime);
+							}
+						}
+					}
+				}
 			}
 		}
 		gdt_free_socket(option);
--- sample/log_reciver/main.c
+++ sample/log_reciver/main.c
@@ -26,7 +26,23 @@
  */
 
 #include "gdt_socket.h"
+#include "gdt_json.h"
 #include "gdt_memory_allocator.h"
+
+GDT_MEMORY_POOL* main_memory_pool = NULL;
+int32_t g_reciver_munit = -1;
+int32_t g_reciver_length = 0;
+
+uint32_t g_recv_line_count = 0;
+uint32_t g_recv_line_count_total = 0;
+
+typedef struct GDT_RECIVER
+{
+	char host[256];
+	char port[32];
+	char target[256];
+	GDT_SOCKET_OPTION* tcp_client;
+} GDT_RECIVER;
 
 int on_connect(GDT_SERVER_CONNECTION_INFO* connection);
 int32_t on_payload_recv(uint32_t payload_type, uint8_t* payload, size_t payload_len, GDT_RECV_INFO *gdt_recv_info);
@@ -36,35 +52,22 @@ int main( int argc, char *argv[], char *envp[] )
 {
 	int exe_code = EX_OK;
 	int result;
-	char hostname[256];
-	char portnum[32];
-	int inetflag = 0;
+	char config_json_file_name[256];
 	gnt_set_argv( &argc, &argv, &envp );
 	do{
-		memset( hostname, 0, sizeof( hostname ) );
-		memset( portnum, 0, sizeof( portnum ) );
-		snprintf( hostname, sizeof( hostname ) -1, "localhost" );
-		snprintf( portnum, sizeof( portnum ) -1, "37012" );
-		while( ( result = getopt( argc, argv, "v:h:p:" ) ) != -1 )
+		if( -1 == gdt_initialize_memory_f64(&main_memory_pool,SIZE_MBYTE*8)){
+			printf( "gdt_initialize_memory_f64 error\n" );
+			break;
+		}
+		memset( config_json_file_name, 0, sizeof( config_json_file_name ) );
+		int optioncount = 0;
+		while( ( result = getopt( argc, argv, "c:" ) ) != -1 )
 		{
 			switch(result)
 			{
-			case 'v':
-				fprintf( stdout,"%c %s\n", result, optarg );
-				if( optarg[0] == '6' ){
-					inetflag = 1;
-				}
-				else if( optarg[0] == '4' ){
-					inetflag = 0;
-				}
-				break;
-			case 'h':
-				fprintf( stdout,"%c %s\n", result, optarg );
-				snprintf( hostname, sizeof( hostname ) -1, "%s", optarg );
-				break;
-			case 'p':
-				fprintf( stdout,"%c %s\n", result, optarg );
-				snprintf( portnum, sizeof( portnum ) -1, "%s", optarg );
+			case 'c':
+				fprintf( stdout,"config : %s\n", optarg );
+				snprintf( config_json_file_name, sizeof( config_json_file_name ) -1, "%s", optarg );
 				break;
 			case ':':
 				fprintf( stdout,"%c needs value\n", result );
@@ -73,19 +76,85 @@ int main( int argc, char *argv[], char *envp[] )
 				fprintf(stdout,"unknown\n");
 				break;
 			}
+			optioncount++;
 		}
-
-		GDT_SOCKET_OPTION* tcp_client = gdt_create_tcp_client(hostname, portnum);
-		set_on_connect_event( tcp_client, on_connect );
-		set_on_payload_recv_event(tcp_client, on_payload_recv);
-		set_on_close_event( tcp_client, on_close );
-		gdt_socket(tcp_client);
+		if( strcmp(config_json_file_name,"") )
+		{
+			GDT_FILE_INFO info;
+			if( 0 == gdt_fget_info( config_json_file_name, &info ) )
+			{
+				int32_t json_string_munit = gdt_create_munit( main_memory_pool, sizeof( char )*info.size+1, MEMORY_TYPE_DEFAULT );
+				if( 0 != gdt_fread( config_json_file_name, (char*)GDT_POINTER(main_memory_pool,json_string_munit), GDT_PUNIT_USIZE(main_memory_pool,json_string_munit) ) ){
+					int32_t root_munit = gdt_json_decode( main_memory_pool, (char*)GDT_POINTER(main_memory_pool,json_string_munit) );
+					GDT_NODE* rootnode = (GDT_NODE*)GDT_POINTER(main_memory_pool,root_munit);
+					GDT_NODE* workelemlist = ( GDT_NODE* )GDT_POINTER( main_memory_pool, rootnode->element_munit );
+					if( workelemlist[0].id == ELEMENT_ARRAY )
+					{
+						int i;
+						GDT_ARRAY* parray;
+						GDT_ARRAY_ELEMENT* elm;
+						parray = (GDT_ARRAY*)GDT_POINTER( main_memory_pool, workelemlist[0].element_munit );
+						elm = (GDT_ARRAY_ELEMENT*)GDT_POINTER( main_memory_pool, parray->munit );
+						g_reciver_munit = gdt_create_munit(main_memory_pool,sizeof(GDT_RECIVER)*parray->len,MEMORY_TYPE_DEFAULT);
+						GDT_RECIVER* recivers = (GDT_RECIVER*)GDT_POINTER(main_memory_pool,g_reciver_munit);
+						g_reciver_length = parray->len;
+						for( i = 0; i < parray->len; i++ )
+						{
+							if( (elm+i)->id == ELEMENT_HASH ){
+								int32_t host_munit = gdt_get_hash(main_memory_pool,(elm+i)->munit,"host");
+								int32_t port_munit = gdt_get_hash(main_memory_pool,(elm+i)->munit,"port");
+								int32_t target_munit = gdt_get_hash(main_memory_pool,(elm+i)->munit,"target");
+								if( host_munit != -1 && port_munit != -1 && target_munit != -1 ){
+									snprintf( (recivers+i)->host, sizeof( (recivers+i)->host ) -1, "%s", (char*)GDT_POINTER(main_memory_pool,host_munit) );
+									snprintf( (recivers+i)->port, sizeof( (recivers+i)->port ) -1, "%s", (char*)GDT_POINTER(main_memory_pool,port_munit) );
+									snprintf( (recivers+i)->target, sizeof( (recivers+i)->target ) -1, "%s", (char*)GDT_POINTER(main_memory_pool,target_munit) );
+									(recivers+i)->tcp_client = gdt_create_tcp_client((recivers+i)->host, (recivers+i)->port);
+									set_message_buffer((recivers+i)->tcp_client,SIZE_MBYTE*4);
+									set_on_connect_event( (recivers+i)->tcp_client, on_connect );
+									set_on_payload_recv_event((recivers+i)->tcp_client, on_payload_recv);
+									set_on_close_event( (recivers+i)->tcp_client, on_close );
+									gdt_socket((recivers+i)->tcp_client);
+									gdt_set_client_id((recivers+i)->tcp_client,i);
+									gdt_client_send_message(0x01, (recivers+i)->target, strlen((recivers+i)->target), (recivers+i)->tcp_client);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		if( g_reciver_munit == -1 )
+		{
+			printf("empty reciver\n");
+			break;
+		}
+		GDT_RECIVER* recivers = (GDT_RECIVER*)GDT_POINTER(main_memory_pool,g_reciver_munit);
+		int32_t sync_timer = time(NULL);
+		int32_t current_time = 0;
+		int sleeptime = 20000;
+		int i;
 		while (1) {
-			gdt_client_update(tcp_client);
-			usleep(1000);
+			for( i = 0; i < g_reciver_length; i++ ){
+				gdt_client_update((recivers+i)->tcp_client);
+				usleep(sleeptime);
+			}
+			current_time = time(NULL);
+			if( g_recv_line_count > 0 ){
+				sleeptime = 10000;
+			}
+			if( current_time - sync_timer > 3 ){
+				if( g_recv_line_count == 0 ){
+					sleeptime = 100000;
+				}
+				sync_timer = current_time;
+				g_recv_line_count_total += g_recv_line_count;
+				g_recv_line_count = 0;
+			}
 		}
-		gdt_free_socket(tcp_client);
-		gdt_free(tcp_client->memory_pool);
+		for( i = 0; i < g_reciver_length; i++ ){
+			gdt_free_socket((recivers+i)->tcp_client);
+			gdt_free((recivers+i)->tcp_client->memory_pool);
+		}
 	}while( false );
 	return exe_code;
 }
@@ -99,11 +168,20 @@ int32_t on_payload_recv(uint32_t payload_type, uint8_t* payload, size_t payload_
 {
 	char *pbuf = (char*)payload;
 	pbuf[payload_len] = '\0';
-	// gdt_print_payload(payload_type,payload,payload_len,128);
+	//gdt_print_payload(payload_type,payload,payload_len,128);
+	int i = 0;
+	for( i = 0; i < payload_len; i++ ){
+		if(pbuf[i] == '\n'){
+			g_recv_line_count++;
+		}
+	}
 	switch( payload_type )
 	{
 		case 0x01:
-			printf("%s",pbuf);
+			//printf("%s",pbuf);
+			//gdt_fwrite_bin_a("./out",pbuf,gdt_strlen(pbuf));
+			printf("recv line(%d) : %d\n", g_recv_line_count, g_recv_line_count_total+g_recv_line_count);
+			gdt_client_send_message(0x02, "1", 1, (GDT_SOCKET_OPTION*)gdt_recv_info->tinfo->gdt_socket_option);
 			break;
 		default:
 			// invalid packet
