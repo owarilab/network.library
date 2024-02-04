b09dc89f43df0fb4c5b1e9341d87e37be30fd03a
k.owari <iamowari@gmail.com>
2024-02-04 12:34:32 +0900

    update

--- plugin/unity/libqs.c
+++ plugin/unity/libqs.c
@@ -7,22 +7,21 @@ int on_http_event(QS_EVENT_PARAMETER params);
 int on_ws_event(QS_EVENT_PARAMETER params);
 int on_close(QS_EVENT_PARAMETER params);
 
-static LIBQS_WORKING_DATA libqs_working_data;
+QS_MEMORY_CONTEXT temporary_memory;
+QS_SERVER_CONTEXT* server_context = 0;
+libqs_api_callback_unity_debug_log debug_log = 0;
+libqs_api_callback_unity_on_recv_http_request on_recv_http_request = 0;
 
 int on_connect(QS_EVENT_PARAMETER params)
 {
-	libqs_api_debug_log("on_connect");
 	return 0;
 }
 
 int on_http_event(QS_EVENT_PARAMETER params)
 {
-	libqs_api_debug_log("on_http_event");
 	int http_status_code = 404;
-	api_qs_memory_clean(&libqs_working_data.temporary_memory);
-	if(libqs_working_data.on_recv_http_request){
-		char* http_response = libqs_working_data.on_recv_http_request(params);
-		http_status_code = api_qs_http_response_raw_json(params,&libqs_working_data.temporary_memory,http_response);
+	if(on_recv_http_request){
+		http_status_code = on_recv_http_request(params);
 	}
 	return http_status_code;
 }
@@ -31,88 +30,106 @@ int on_ws_event(QS_EVENT_PARAMETER params)
 {
 	char* message = api_qs_get_ws_message(params);
 	api_qs_send_ws_message(params,message);
-	//api_qs_send_ws_message_plane(params,message);
 	return 0;
 }
 
 int on_close(QS_EVENT_PARAMETER params)
 {
-	libqs_api_debug_log("on_close");
 	return 0;
 }
 
 UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_init()
 {
-	memset(&libqs_working_data,0,sizeof(LIBQS_WORKING_DATA));
-    libqs_working_data.main_memory.memory = 0;
-    libqs_working_data.temporary_memory.memory = 0;
-	libqs_working_data.server_context = 0;
-    libqs_working_data.debug_log = 0;
-	libqs_working_data.on_recv_http_request = 0;
-    api_qs_memory_alloc(&libqs_working_data.temporary_memory,1024*1024*32);
-    api_qs_memory_alloc(&libqs_working_data.main_memory,1024*1024*64);
+	api_qs_memory_alloc(&temporary_memory,1024*1024*4);
     return api_qs_init();
 }
 
 UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_init_http_server(uint32_t server_port, uint32_t max_connection)
 {
-	libqs_api_debug_log("libqs_api_init_http_server");
-	if(0 > api_qs_server_init(&libqs_working_data.server_context,server_port,max_connection)){return -1;}
+	if(0 > api_qs_server_init(&server_context,server_port,max_connection)){return -1;}
 	//if(-1==api_qs_server_create_router(libqs_working_data.server_context)){return -1;}
 	//if(-1==api_qs_server_create_kvs(libqs_working_data.server_context,QS_KVS_MEMORY_TYPE_B1MB)){return -1;}
 	//if(-1==api_qs_server_create_logger_access(libqs_working_data.server_context,"./access_log.txt")){return -1;}
 	//if(-1==api_qs_server_create_logger_debug(libqs_working_data.server_context,"./debug_log.txt")){return -1;}
 	//if(-1==api_qs_server_create_logger_error(libqs_working_data.server_context,"./error_log.txt")){return -1;}
-	api_qs_set_on_connect_event(libqs_working_data.server_context, on_connect );
-	api_qs_set_on_http_event(libqs_working_data.server_context, on_http_event );
-	api_qs_set_on_websocket_event(libqs_working_data.server_context, on_ws_event );
-	api_qs_set_on_close_event(libqs_working_data.server_context, on_close );
+	api_qs_set_on_connect_event(server_context, on_connect );
+	api_qs_set_on_http_event(server_context, on_http_event );
+	api_qs_set_on_websocket_event(server_context, on_ws_event );
+	api_qs_set_on_close_event(server_context, on_close );
 	return 0;
 }
 
 UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_update()
 {
-	api_qs_update(libqs_working_data.server_context);
+	api_qs_update(server_context);
 	return 0;
 }
 
 UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_free()
 {
-	if(libqs_working_data.server_context){
-		api_qs_free(libqs_working_data.server_context);
+	api_qs_memory_free(&temporary_memory);
+	debug_log = 0;
+	on_recv_http_request = 0;
+	if(server_context!=0){
+		api_qs_free(server_context);
 	}
-	if(libqs_working_data.temporary_memory.memory){
-		api_qs_memory_free(&libqs_working_data.temporary_memory);
-	}
-	if(libqs_working_data.main_memory.memory){
-		api_qs_memory_free(&libqs_working_data.main_memory);
-	}
-    libqs_working_data.debug_log = 0;
-	libqs_working_data.on_recv_http_request = 0;
 	return 0;
 }
 
 UNITY_INTERFACE_EXPORT uint32_t UNITY_INTERFACE_API libqs_api_rand()
 {
-    libqs_api_debug_log("libqs_api_rand");
     return api_qs_rand();
 }
 
 UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API libqs_api_set_debug_log(libqs_api_callback_unity_debug_log callback)
 {
-    libqs_working_data.debug_log = callback;
+    debug_log = callback;
 }
 
 UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API libqs_api_set_on_recv_http_request(libqs_api_callback_unity_on_recv_http_request callback)
 {
-	libqs_working_data.on_recv_http_request = callback;
+	on_recv_http_request = callback;
 }
 
-void libqs_api_debug_log(const char* str)
+UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_send_response(void* http_request_parameter,char* response)
 {
-    if (libqs_working_data.debug_log)
+	api_qs_send_response(http_request_parameter, response);
+	return 0;
+}
+
+UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_send_http_response_json(void* http_request_parameter,char* json)
+{
+	api_qs_memory_clean(&temporary_memory);
+	return api_qs_http_response_raw_json(http_request_parameter,&temporary_memory,json);
+}
+
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_method(void* http_request_parameter)
+{
+	return api_qs_get_http_method(http_request_parameter);
+}
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_path(void* http_request_parameter)
+{
+	return api_qs_get_http_path(http_request_parameter);
+}
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_get_parameter(void* http_request_parameter, char* name)
+{
+	return api_qs_get_http_get_parameter(http_request_parameter, name);
+}
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_APIlibqs_api_get_http_post_parameter(void* http_request_parameter, char* name)
+{
+	return api_qs_get_http_post_parameter(http_request_parameter, name);
+}
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_post_body(void* http_request_parameter)
+{
+	return api_qs_get_http_post_body(http_request_parameter);
+}
+
+
+void libqs_api_debug_log(char* str)
+{
+    if (debug_log)
     {
-        libqs_working_data.debug_log(str);
+        debug_log(str);
     }
 }
 
--- plugin/unity/libqs.h
+++ plugin/unity/libqs.h
@@ -1,19 +1,30 @@
+
+#ifdef _WINDOWS
 #define UNITY_INTERFACE_API __stdcall
 #define UNITY_INTERFACE_EXPORT __declspec(dllexport)
+#else
+//#define UNITY_INTERFACE_API __attribute__((cdecl))
+#define UNITY_INTERFACE_API 
+#define UNITY_INTERFACE_EXPORT 
+#endif
+
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+#ifdef _WINDOWS
 #pragma comment(lib,"ws2_32.lib")
+#endif
+
 #include "qs_api.h"
 
 // unity c#側にchar*を受け取る関数の関数ポインタの宣言
-typedef void(UNITY_INTERFACE_API *libqs_api_callback_unity_debug_log)(const char* str);
+typedef void(UNITY_INTERFACE_API *libqs_api_callback_unity_debug_log)(char* str);
 
-// unity c#側にvoid*を受け取り、char*を返す関数の関数ポインタの宣言
-typedef char* (UNITY_INTERFACE_API *libqs_api_callback_unity_on_recv_http_request)(void* http_request_parameter);
+// unity c#側にvoid*を受け取り、intを返す関数の関数ポインタの宣言
+typedef int (UNITY_INTERFACE_API *libqs_api_callback_unity_on_recv_http_request)(void* http_request_parameter);
 
 typedef struct {
     QS_MEMORY_CONTEXT main_memory;
@@ -30,7 +41,16 @@ UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_free();
 UNITY_INTERFACE_EXPORT uint32_t UNITY_INTERFACE_API libqs_api_rand();
 UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API libqs_api_set_debug_log(libqs_api_callback_unity_debug_log callback);
 UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API libqs_api_set_on_recv_http_request(libqs_api_callback_unity_on_recv_http_request callback);
-void libqs_api_debug_log(const char* str);
+UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_send_response(void* http_request_parameter,char* response);
+UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API libqs_api_send_http_response_json(void* http_request_parameter,char* json);
+
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_method(void* http_request_parameter);
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_path(void* http_request_parameter);
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_get_parameter(void* http_request_parameter, char* name);
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_post_parameter(void* http_request_parameter, char* name);
+UNITY_INTERFACE_EXPORT char* UNITY_INTERFACE_API libqs_api_get_http_post_body(void* http_request_parameter);
+
+void libqs_api_debug_log(char* str);
 
 #ifdef __cplusplus
 }
