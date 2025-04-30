cc5a17fa76aaddcce82c44391084fc34258f7889
k.owari <iamowari@gmail.com>
2025-04-30 17:32:14 +0900

    update

--- sample/client_simple/main.c
+++ sample/client_simple/main.c
@@ -40,83 +40,27 @@ int main( int argc, char *argv[], char *envp[] )
 #ifdef __WINDOWS__
 	SetConsoleOutputCP(CP_UTF8);
 #endif
-
-	QS_HTTP_CLIENT_CONTEXT context;
-	memset(&context, 0, sizeof(context));
-	const char* server_host = "datatracker.ietf.org";
-	const char* request_path = "/doc/html/rfc6455";
-	int server_port = 443;
-	if(0 != api_qs_http_client_connect(&context,server_host,server_port,1)){
-		printf("api_qs_http_client_connect error\n");
-		return -1;
-	}
-
-	snprintf(context.request_buffer,sizeof(context.request_buffer),
-		"GET %s HTTP/1.1\r\n"
-		"Host: %s\r\n"
-		"Connection: close\r\n"
-		"\r\n",
-		request_path,
-		server_host);
-
-	printf("connecting start...\n");
-
-	while(1){
-		api_qs_http_client_update(&context);
-		if(context.phase == QS_SSL_MODULE_PHASE_DISCONNECT){
-			break;
+    const char* server_host = "localhost";
+	int server_port = 52001;
+	QS_CLIENT_CONTEXT* context = 0;
+    int error = 0;
+	if(0 != (error=api_qs_client_init(&context,server_host,server_port))){
+        printf("api_qs_client_init error:%d\n",error);
+        return -1;
+    }
+	api_qs_set_client_on_connect_event(context, on_connect );
+	api_qs_set_client_on_plain_event(context, on_recv );
+	api_qs_set_client_on_close_event(context, on_close );
+	int timer = time(0);
+	for(;;){
+		api_qs_client_update(context);
+		api_qs_client_sleep(context);
+		if(time(0) - timer > 3){
+			api_qs_client_send(context,"test1",5);
+			timer = time(0);
 		}
-		api_qs_client_sleep(context.client_context);
 	}
-	api_qs_http_client_free(&context);
-
-	printf("qs_client_simple_result\n");
-	printf("header:\n%s\n",context.header_buffer);
-
-	char http_version[16];
-	int status_code;
-	char status_message[1024];
-	sscanf(context.header_buffer,"HTTP/%s %d %s\r\n",http_version,&status_code,status_message);
-	printf("http_version:%s\n",http_version);
-	printf("status_code:%d\n",status_code);
-	printf("status_message:%s\n",status_message);
-
-	char content_type[1024];
-	api_qs_http_client_get_header(&context,"Content-Type: ",content_type,sizeof(content_type));
-	printf("content_type: %s\n", content_type);
-
-	char date[1024];
-	api_qs_http_client_get_header(&context,"Date: ",date,sizeof(date));
-	printf("date: %s\n", date);
-
-	char content_length[1024];
-	memset(content_length,0,sizeof(content_length));
-	api_qs_http_client_get_header(&context,"Content-Length: ",content_length,sizeof(content_length));
-	printf("content_length: %s\n", content_length);
-
-	//printf("body:\n%s\n",context.body_buffer);
-
-    //const char* server_host = "localhost";
-	//int server_port = 52001;
-	//QS_CLIENT_CONTEXT* context = 0;
-    //int error = 0;
-	//if(0 != (error=api_qs_client_init(&context,server_host,server_port))){
-    //    printf("api_qs_client_init error:%d\n",error);
-    //    return -1;
-    //}
-	//api_qs_set_client_on_connect_event(context, on_connect );
-	//api_qs_set_client_on_plain_event(context, on_recv );
-	//api_qs_set_client_on_close_event(context, on_close );
-	//int timer = time(0);
-	//for(;;){
-	//	api_qs_client_update(context);
-	//	api_qs_client_sleep(context);
-	//	if(time(0) - timer > 3){
-	//		api_qs_client_send(context,"test1",5);
-	//		timer = time(0);
-	//	}
-	//}
-	//api_qs_client_free(context);
+	api_qs_client_free(context);
 	return 0;
 }
 
